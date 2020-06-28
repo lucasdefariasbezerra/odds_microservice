74bef87cd790b5d80c753e90d12e5235ea547d3a
lucasdefariasbezerra <lucasfbezerra@hotmail.com>
2020-06-28 02:44:26 -0300

    added flyway to integrate database

---
+++ src/main/resources/db/migration/V1__Create_sport_table.sql
@@ -0,0 +1,21 @@
+CREATE TABLE sport (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+INSERT INTO sport VALUES
+(1,'soccer'),
+(2,'basketball'),
+(3,'beiseball'),
+(4,'football'),
+(5,'tennis'),
+(6,'volley'),
+(7,'MMA'),
+(8,'boxe'),
+(9,'athletism'),
+(10,'automobilism'),
+(11,'hockey'),
+(12,'e-sports'),
+(13,'cricket'),
+(14,'swimming');
